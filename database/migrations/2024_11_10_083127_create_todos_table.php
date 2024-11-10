65136b47336bc8ff0b102a7bffad3d06f19eb92f
toriworx <torikatachi1994@gmail.com>
2024-11-10 19:06:01 +0900

    テーブルからcontentカラムの削除

--- database/migrations/2024_11_10_083127_create_todos_table.php
+++ database/migrations/2024_11_10_083127_create_todos_table.php
@@ -15,7 +15,6 @@ class CreateTodosTable extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
-            $table->string('content');
             $table->timestamps();
         });
     }
