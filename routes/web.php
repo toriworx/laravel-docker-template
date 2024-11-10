719ab0b8f6a6777f7015edbadf3aeaabd50524d6
toriworx <torikatachi1994@gmail.com>
2024-11-10 22:45:09 +0900

    add:一覧画面を表示の追加

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('/todo', function () {
-    echo 'Hello World!';
-});
+use App\Http\Controllers\TodoController;
+Route::get('/todo', [TodoController::class, 'index']);
